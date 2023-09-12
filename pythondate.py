895e8a78d6c44f5173f99dd437a4e3bdd24d3240
JerryNYang <46272314+JerryNYang@users.noreply.github.com>
2023-09-12 17:18:25 -0500

    Create pythondate.py

---
+++ pythondate.py
@@ -0,0 +1,6 @@
+# module: sys
+# very important to hackers
+import sys #system functions and parameters
+from datetime import datetime as dt # import with alias
+# print(datetime.now()) # without as dt in import
+print(dt.now()) #... as dt in import
