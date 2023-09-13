5933049a5db926a3bf7e2b62d0bedee51ed2df51
JerryNYang <46272314+JerryNYang@users.noreply.github.com>
2023-09-13 15:23:19 -0500

    Create mini_calculator.py

---
+++ mini_calculator.py
@@ -0,0 +1,21 @@
+
+# jerry inyang
+# mini calculator
+#USER INPUT
+
+x = float(input("Give me a number: "))
+ = input("Give me an operator: ")
+y = float(input("Give me yet another number: "))
+
+if o == "+":
+	print(x + y)
+elif o == "-":
+	print(x - y)
+elif o == "/":
+	print(x / y)
+elif o == "*":
+	print(x * y)
+elif o == "**":
+	print(x ** y)
+else:
+	print("Unknown operator.")
