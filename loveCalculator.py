b6cbe3ca2dbec12cc9355e9e55e8562d1d0bd93b
JerryNYang <46272314+JerryNYang@users.noreply.github.com>
2024-02-15 06:46:10 -0600

    Update loveCalculator.py

--- loveCalculator.py
+++ loveCalculator.py
@@ -2,7 +2,7 @@
 #python
 # 9.26.23
 
-print("Welcome to the Love Calculator!")
+print("Welcome to the Love Calculator!!!")
 name1 = input("What is your name? \n")
 name2 = input("What is their name? \n")
 
